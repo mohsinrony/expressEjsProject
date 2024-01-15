5a8c7aa060a531e721413d2dcad40edc19a8baac
[mdmohsin] <[roni07wd@gmail.com]>
2024-01-15 13:20:01 +0200

    define route for handling HTTP POST req and make the server to listen for incoming HTTP requests

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'pageTemplates'));
 
 const homePath = path.join(__dirname, 'home.html');
+
+app.get('/', (req, res) => {res.sendFile(homePath);});
+app.post('/login', express.urlencoded({extended:false}),(req, res) => {console.log(req.body);});
+
+app.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`);
+})
