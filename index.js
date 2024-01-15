5bd28348c2cb7fd2c8d457abb8fc43e2055193bc
[mdmohsin] <[roni07wd@gmail.com]>
2024-01-15 13:06:17 +0200

    create a form in home.html

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const express = require('express');
 const app = express();
 
 const {port, host} = require('./config');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'pageTemplates'));
+
+const homePath = path.join(__dirname, 'home.html');
