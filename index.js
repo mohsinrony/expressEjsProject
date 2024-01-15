f7bf12f8731a34d300772039add317dd890a80b4
[mdmohsin] <[roni07wd@gmail.com]>
2024-01-15 13:48:42 +0200

    add result page

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, 'pageTemplates'));
 const homePath = path.join(__dirname, 'home.html');
 
 app.get('/', (req, res) => {res.sendFile(homePath);});
-app.post('/login', express.urlencoded({extended:false}),(req, res) => {console.log(req.body);});
+app.post('/login', express.urlencoded({extended:false}),(req, res) => {console.log(req.body);}); // express.urlenconded() creates the body object
 
 app.listen(port, host, () => {
     console.log(`Server running at http://${host}:${port}/`);
